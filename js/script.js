2a6d8e23d7eaa3554509812332ca60e808c0ba96
Andre <andreirie10@gmail.com>
2025-07-07 00:52:37 -0300

    Final ver.

--- js/script.js
+++ js/script.js
@@ -27,14 +27,12 @@ closeBtn.addEventListener('click', () => {
   sideMenu.classList.remove('active');
 });
 
-// Fecha o menu ao clicar fora dele
 document.addEventListener('click', (e) => {
   const isClickInside = sideMenu.contains(e.target) || menuIcon.contains(e.target);
   if (!isClickInside) {
     sideMenu.classList.remove('active');
   }
 });
-
 
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("preVendaForm");
@@ -49,16 +47,24 @@ document.addEventListener("DOMContentLoaded", function () {
     if (!validateEmail(email)) {
         message.textContent = "Por favor, insira um e-mail válido.";
         message.className = "form-message error";
+        messageTimeout = setTimeout(() => {
+          message.textContent = "";
+          message.className = "form-message";
+        }, 3000);
         return;
     }
 
     message.textContent = "Inscrito com sucesso!";
     message.className = "form-message success";
 
+    messageTimeout = setTimeout(() => {
+      message.textContent = "";
+      message.className = "form-message";
+    }, 3000);
   });
 
     function validateEmail(email) {
-        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return re.test(email.toLowerCase());
-  }
+      const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+      return re.test(email.toLowerCase());
+    }
 });
